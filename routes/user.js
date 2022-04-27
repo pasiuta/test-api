e7e44369c9c5c9d4512a8aad35c1cfcc598bfd20
Dmytro Pasiuta <dmitriy@Dmytros-MacBook-Pro.local>
2022-04-27 18:18:00 +0300

    init commit

--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,10 @@ const router = new Router();
 const userController = require('../controller/user');
 const authController = require('../controller/auth');
 
-router.post('/user', userController.createUser);
 router.post('/login', authController.login);
+router.post('/user', userController.createUser);
 router.get('/user/:id', userController.getUser);
 router.put('/user', userController.updateUser);
+
 
 module.exports = router;
